962f461ffa93fd89a0b8ec09cedaaf7b4626808a
Pawel Lomza <pawel.lomza4@gmail.com>
2023-07-05 18:19:19 +0200

    19.3 Implementation of functionality

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -1,11 +1,13 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.controller.TaskNotFoundException;
 import com.crud.tasks.domain.Task;
 import com.crud.tasks.repository.TaskRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,8 +19,12 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task findById(long id){
-        return repository.findById(id);
+    public Task getTask(final Long task) throws TaskNotFoundException {
+        return repository.findById(task).orElseThrow(TaskNotFoundException::new);
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
     }
 
 }
